1577802560c540367864783c99a20c2d01330c74
yohanmishkin <yohanmishkin@protonmail.com>
2024-03-10 23:35:06 -0400

    Scribbling with ISR

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,6 +1,81 @@
+import Link from "next/link";
 import Image from "next/image";
+import React from "react";
+import { parse } from "node-html-parser";
 
-export default function Page() {
+const getRandom = function <T>(arr: Array<T>): T {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const withHost = (path: string) => `https://gallerix.org${path}`;
+
+const withHttps = (path: string) => `https:${path}`;
+
+async function getDocument(url: string) {
+  let response = await fetch(url);
+
+  let html = await response.text();
+
+  return parse(html);
+}
+
+async function getRandomPicture(): Promise<string> {
+  let letters = ["_RUS", ..."ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")];
+
+  let randomLetter = getRandom(letters);
+
+  let randomLetterUrl = `https://gallerix.org/storeroom/letter/${randomLetter}/`;
+
+  let randomLetterDocument = await getDocument(randomLetterUrl);
+
+  let artists = Array.from(
+    randomLetterDocument.querySelectorAll('.sr-pntrs [href^="/storeroom/"]'),
+  );
+
+  let randomArtist = getRandom(artists);
+
+  function isElement(qwer: unknown): asserts qwer is Element {
+    if (qwer === null) {
+      throw new Error("Failed picking a random artist");
+    }
+  }
+
+  isElement(randomArtist);
+
+  let artistUrl = withHost(
+    randomArtist.getAttribute("href") ?? "NO ARTIST LINK!",
+  );
+
+  let randomArtistDocument = await getDocument(artistUrl);
+
+  let thumbnailLinks = Array.from(
+    randomArtistDocument.querySelectorAll(".pic .animsition-link"),
+  );
+
+  let randomThumbnail = getRandom(thumbnailLinks);
+  isElement(randomThumbnail);
+
+  let imageUrl = withHost(
+    randomThumbnail.getAttribute("href") ?? "NO THUMBNAIL!",
+  );
+
+  let imageDocument = await getDocument(imageUrl);
+
+  let jpegUrl =
+    imageDocument?.querySelector("#xpic")?.getAttribute("src") ?? "NO LUCK";
+
+  return jpegUrl;
+}
+
+async function getRandomPictures(num: number): Promise<Array<string>> {
+  const results: Array<string> = [];
+  for (let i = 0; i < num; i++) {
+    // results.push(await getRandomPicture());
+  }
+  return results;
+}
+
+export default async function Page() {
   return (
     <div>
       <h1>Hello!</h1>
@@ -11,6 +86,10 @@ export default function Page() {
         alt="Cool picture"
         data-test="picture"
       />
+
+      <Link href={`/explore/abc-1234-def`} data-test="begin">
+        Begin
+      </Link>
     </div>
   );
 }
--- cypress/e2e/acceptance.cy.ts
+++ cypress/e2e/acceptance.cy.ts
@@ -1,5 +1,14 @@
 it("displays an image", () => {
   cy.visit("/explore");
 
+  cy.get(`[data-test="begin"]`).click();
+
+  cy.location("href").should(
+    "match",
+    /explore\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+  );
+
   cy.get(`[data-test="picture"]`).should("have.attr", "src", "/vercel.svg");
+
+  // cy.get(`[data-test="next"]`).click();
 });
