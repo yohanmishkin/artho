ebb0a571f7debd72a2458c658d68ace6a5176462
yohanmishkin <yohanmishkin@protonmail.com>
2024-03-15 23:13:44 -0400

    UUUID vs string name

--- app/explore/[id]/page.tsx
+++ app/explore/[id]/page.tsx
@@ -9,11 +9,20 @@ type RandomImage = {
   src: string;
 };
 
+function createUUID() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c: any) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16),
+  );
+}
+
 export async function generateStaticParams(): Promise<Array<RandomImage>> {
   const getPictures = (n: number) =>
     Array(n)
       .fill(null)
-      .map((x, i) => `picture${i}`);
+      .map((x, i) => createUUID());
 
   const pictures = await Promise.resolve(getPictures(10));
 
