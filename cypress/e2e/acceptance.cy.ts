f7296195b3b320ff25f0c26ab646ae9386e69cdc
yohanmishkin <yohanmishkin@protonmail.com>
2024-03-16 22:14:12 -0400

    TEST: More test sketching

--- cypress/e2e/acceptance.cy.ts
+++ cypress/e2e/acceptance.cy.ts
@@ -1,19 +1,29 @@
 export {};
 
-it("displays an image and pre-fetches the next", () => {
-  cy.intercept("/_next/image?url**", {});
+it('displays an image and pre-fetches the next', () => {
+  // cy.intercept('/_next/image?url**', {});
 
-  cy.visit("/explore");
+  let firstImage: string | null = null;
+  let nextImage: string | null = null;
+
+  cy.visit('/explore');
 
   cy.get(`[data-test="picture"]`)
-    .should("have.attr", "src")
-    .should("match", /_next\/image\?url/);
+    .should('have.attr', 'src')
+    .should('match', /_next\/image\?url/)
+    .as('firstImage');
 
   cy.get(`[data-test="next-picture"]`)
-    .should("have.attr", "src")
-    .should("match", /_next\/image\?url/);
+    .should('have.attr', 'src')
+    .should('match', /_next\/image\?url/)
+    .as('nextImage');
 
-  // scroll
+  cy.get(`[data-test="next-picture"]`).scrollIntoView();
+
+  cy.get('@firstImage').then(() => {
+    expect(firstImage).equal(nextImage);
+  });
+  // cy.get(`[data-test="picture"]`).should('have.attr', 'src', '@nextImage');
 
   // should fetch another picture
 
