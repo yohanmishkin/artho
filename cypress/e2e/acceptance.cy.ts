083b5b59fb2eda830df59ce5fa69423bd0f3e992
yohanmishkin <yohanmishkin@protonmail.com>
2024-04-15 22:03:17 -0400

    TEST: SCRAP

--- cypress/e2e/acceptance.cy.ts
+++ cypress/e2e/acceptance.cy.ts
@@ -1,33 +1,40 @@
 export {};
 
 it('displays an image and pre-fetches the next', () => {
-  // cy.intercept('/_next/image?url**', {});
-
-  let firstImage: string | null = null;
-  let nextImage: string | null = null;
+  cy.intercept('/_next/image?url**', {});
 
   cy.visit('/explore');
 
   cy.get(`[data-test="picture"]`)
     .should('have.attr', 'src')
     .should('match', /_next\/image\?url/)
-    .as('firstImage');
+    .then((image1) => {
+      cy.get(`[data-test="next-picture"]`)
+        .should('have.attr', 'src')
+        .should('match', /_next\/image\?url/)
+        .then((image2) => {
+          expect(image2).not.equal(image1);
 
-  cy.get(`[data-test="next-picture"]`)
-    .should('have.attr', 'src')
-    .should('match', /_next\/image\?url/)
-    .as('nextImage');
+          cy.get(`[data-test="next-picture"]`)
+            .scrollIntoView()
+            .then(() => {
+              cy.get(`[data-test="next-picture"]`)
+                .should('have.attr', 'src')
+                .should('match', /_next\/image\?url/)
+                .then((image3) => {
+                  expect(image3).not.equal(image2);
+                });
+            });
+        });
 
-  cy.get(`[data-test="next-picture"]`).scrollIntoView();
+      // cy.get(`[data-test="picture"]`)
+      //   .should('have.attr', 'src')
+      //   .then((currentImage) => {
+      //     debugger;
+      //   });
+    });
 
-  cy.get('@firstImage').then(() => {
-    expect(firstImage).equal(nextImage);
-  });
-  // cy.get(`[data-test="picture"]`).should('have.attr', 'src', '@nextImage');
-
-  // should fetch another picture
-
-  // scroll
-
-  // should fetch another picture
+  // cy.get('@firstImage').then(() => {
+  // expect(firstImage).equal(nextImage);
+  // });
 });
