a37474ac01e0a4331c4aa0d441a756613fe09041
yohanmishkin <yohanmishkin@protonmail.com>
2024-04-16 23:03:34 -0400

    FILE: Moved data fetching out of page component

---
+++ app/explore/data.ts
@@ -0,0 +1,71 @@
+import { parse } from 'node-html-parser';
+
+function getRandom<T>(arr: Array<T>): T {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const withHost = (path: string) => `https://gallerix.org${path}`;
+
+async function getDocument(url: string) {
+  let response = await fetch(url);
+
+  let html = await response.text();
+
+  return parse(html);
+}
+
+export async function getRandomPicture(): Promise<string> {
+  let letters = ['_RUS', ...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')];
+
+  let randomLetter = getRandom(letters);
+
+  let randomLetterUrl = `https://gallerix.org/storeroom/letter/${randomLetter}/`;
+
+  let randomLetterDocument = await getDocument(randomLetterUrl);
+
+  let artists = Array.from(
+    randomLetterDocument.querySelectorAll('.sr-pntrs [href^="/storeroom/"]'),
+  );
+
+  let randomArtist = getRandom(artists);
+
+  function isElement(qwer: unknown): asserts qwer is Element {
+    if (qwer === null) {
+      throw new Error('Failed picking a random artist');
+    }
+  }
+
+  isElement(randomArtist);
+
+  let artistUrl = withHost(
+    randomArtist.getAttribute('href') ?? 'NO ARTIST LINK!',
+  );
+
+  let randomArtistDocument = await getDocument(artistUrl);
+
+  let thumbnailLinks = Array.from(
+    randomArtistDocument.querySelectorAll('.pic .animsition-link'),
+  );
+
+  let randomThumbnail = getRandom(thumbnailLinks);
+  isElement(randomThumbnail);
+
+  let imageUrl = withHost(
+    randomThumbnail.getAttribute('href') ?? 'NO THUMBNAIL!',
+  );
+
+  let imageDocument = await getDocument(imageUrl);
+
+  let jpegUrl =
+    imageDocument?.querySelector('#xpic')?.getAttribute('src') ?? 'NO LUCK';
+
+  return jpegUrl;
+}
+
+async function getRandomPictures(num: number): Promise<Array<string>> {
+  const results: Array<string> = [];
+  for (let i = 0; i < num; i++) {
+    // results.push(await getRandomPicture());
+  }
+  return results;
+}
